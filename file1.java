7ebd8751c70778361ed028e9ae9a89f23dd3a8b7
Shek Ismail <shekismailmvj@gmail.com>
2023-02-11 23:51:48 +0530

    DMS-98098

--- file1.java
+++ file1.java
@@ -2,5 +2,9 @@ package myFirstRepo;
 class test {
     public static void main(String[] args){
     System.out.println("Yes");
+    System.out.println("How's life?");
+    int a = 120;int b=75;
+int c = b-a;
+System.out.print(c);
     
 }}
